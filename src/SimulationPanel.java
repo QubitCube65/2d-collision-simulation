69fd76aa98c92962c64df1e456abfd05f5b99d0f
Amelie <amelie.kannewitz@gmail.com>
2025-05-30 09:57:43 +0200

    added gradient to balls

--- src/SimulationPanel.java
+++ src/SimulationPanel.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.awt.RadialGradientPaint;
+import java.awt.geom.Point2D;
 
 public class SimulationPanel extends JPanel{
     private List<Ball> balls;
@@ -16,14 +18,27 @@ public class SimulationPanel extends JPanel{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //edge straightening
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //edge smoothening
 
         int groundHeight = 20;
         g2d.setColor(new Color(200,200,200));
-        //g2d.fillRect(0, getHeight() - groundHeight, getWidth(), groundHeight);
+        // g2d.fillRect(0, getHeight() - groundHeight, getWidth(), groundHeight);
 
-        for(Ball b : balls) {
-            g2d.setColor(b.color);
+        for (Ball b : balls) {
+            float centerX = (float) b.x;
+            float centerY = (float) b.y;
+            float radius = (float) b.radius;
+
+            Color centerColor = b.color.brighter();
+            Color edgeColor = b.color;
+
+            RadialGradientPaint paint = new RadialGradientPaint(
+                    new Point2D.Float(centerX, centerY),
+                    radius,
+                    new float[] {0f, 1f},
+                    new Color[] {centerColor, edgeColor}
+            );
+            g2d.setPaint(paint);
             g2d.fillOval(
                     (int)(b.x - b.radius),
                     (int)(b.y - b.radius),
@@ -33,3 +48,25 @@ public class SimulationPanel extends JPanel{
         }
     }
 }
+
+/**
+ * @Override
+ *     protected void paintComponent(Graphics g) {
+ *         super.paintComponent(g);
+ *         Graphics2D g2d = (Graphics2D) g;
+ *         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //edge straightening
+ *
+ *         int groundHeight = 20;
+ *         g2d.setColor(new Color(200,200,200));
+ *         //g2d.fillRect(0, getHeight() - groundHeight, getWidth(), groundHeight);
+ *
+ *         for(Ball b : balls) {
+ *             g2d.setColor(b.color);
+ *             g2d.fillOval(
+ *                     (int)(b.x - b.radius),
+ *                     (int)(b.y - b.radius),
+ *                     (int)(2 * b.radius),
+ *                     (int)(2 * b.radius)
+ *             );
+ *         }
+ *     }*/
